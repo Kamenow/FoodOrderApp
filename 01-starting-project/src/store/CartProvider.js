ba2858b2d3553d17d6b9244b1116f5a805626f2e
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-01-27 22:53:08 +0200

    fixed cart logic

--- 01-starting-project/src/store/CartProvider.js
+++ 01-starting-project/src/store/CartProvider.js
@@ -9,9 +9,28 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   switch (action.type) {
     case 'ADD':
-      const updatedItems = state.items.concat(action.item);
       const updatedTotalAmount =
         state.totalAmount + action.item.price * action.item.amount;
+
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+
+      const existingCartItem = state.items[existingCartItemIndex];
+      //   let updatedItem;
+      let updatedItems;
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.item.amount,
+        };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        // updatedItem = { ...action.item };
+        updatedItems = state.items.concat(action.item);
+      }
 
       return { items: updatedItems, totalAmount: updatedTotalAmount };
     case 'REMOVE':
