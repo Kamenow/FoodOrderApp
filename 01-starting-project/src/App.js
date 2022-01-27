a63c5bd88387e7a44485fad837d97108486dea98
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-01-27 17:50:36 +0200

    added portals for cart

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from 'react';
+
 import Header from './components/Layout/Header.js';
 import Meals from './components/Meals/Meals.js';
+import Cart from './components/Cart/Cart.js';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
