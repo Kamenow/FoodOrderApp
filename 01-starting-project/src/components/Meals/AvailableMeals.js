9eedb0029ec392a7f526295a1495240575ba11f5
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-01-27 16:31:18 +0200

    added meal Item

--- 01-starting-project/src/components/Meals/AvailableMeals.js
+++ 01-starting-project/src/components/Meals/AvailableMeals.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card.js';
+import MealItem from './MealItem/MealItem.js';
 
 const DUMMY_MEALS = [
   {
@@ -30,11 +32,20 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
