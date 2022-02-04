bc409038357d0df56ad1d6b2073fe3892cde5cd1
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-02-04 15:34:27 +0200

    added loading and error state

--- 01-starting-project/src/components/Meals/AvailableMeals.js
+++ 01-starting-project/src/components/Meals/AvailableMeals.js
@@ -6,12 +6,18 @@ import MealItem from './MealItem/MealItem.js';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         'https://react-http-edc05-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
       const responseData = await response.json();
 
@@ -27,10 +33,30 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(`${error.message} 😢`);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>...Loading</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
